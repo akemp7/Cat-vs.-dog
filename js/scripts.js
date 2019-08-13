60b3fc82ae5a93ffa84b10bf13d495c56a331507
Anita Kemp and Neha Malhotra <neha@>
2019-08-13 15:16:04 -0700

    adding dialogues and toggle function

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,24 @@ $(document).ready(function() {
   $("button#catbutton").click(function() {
     $("ul#list1").prepend("<li>how r u?</li>");
     $("ul#list2").prepend("<li>am good</li>");
-  } );
+  });
 });
 
 $(document).ready(function() {
   $("button#dogbutton").click(function() {
     $("ul#list1").prepend("<li>Meow</li>");
-$("ul#list2").prepend("<li>Ruff</li>");
-  } );
+    $("ul#list2").prepend("<li>Ruff</li>");
+  });
+});
+
+$(document).ready(function() {
+  $("img#cat").click(function() {
+    $("#img1").toggle(2000);
+  });
+});
+
+$(document).ready(function() {
+  $("img#dog").click(function() {
+    $("#img2").toggle(2000);
+  });
 });
